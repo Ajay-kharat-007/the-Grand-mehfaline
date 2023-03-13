7a258df3a1760798b16c42aa3f2cbbdcb7e8cae4
Ajay-kharat-007 <kharatajay023@gmail.com>
2023-03-13 16:56:31 +0530

    some changekljkdj

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  constructor(private http:HttpClient){
+  }
+
+  getVal(data: any) {
+    this.http.post('http://localhost:3000/users',data)
+    console.log(data)
+  }
 }
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms'
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
   declarations: [
-    SignupComponent
+    SignupComponent,
+    
   ],
   imports: [
     CommonModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class SignupModule { }
